d52ccd1d7e241418e166a0b0e4dd35623503bbb5
Ryan Brinkman <ryanjbrinkman@outlook.com>
2024-11-09 18:44:30 -0600

    Initial add of project 'to-do-list-app.'

---
+++ intellij/to-do-list-app/src/Main.java
@@ -0,0 +1,19 @@
+/*
+ * ---------------------------------------------------
+ * Project: To Do List Application
+ * Author: Ryan Brinkman
+ * Date: November 7, 2024
+ * Repository: https://github.com/ryanbrin/java/
+ * ---------------------------------------------------
+ * Description:
+ * This application helps users manage their daily tasks by
+ * providing features such as adding, editing, deleting, and
+ * marking tasks as complete. It also includes task filtering
+ * for viewing pending and completed tasks.
+ * ---------------------------------------------------
+ */
+
+public class App {
+    public static void main(String[] args) {
+    }
+}
