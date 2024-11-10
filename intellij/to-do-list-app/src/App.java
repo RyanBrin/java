0c85c49f53a18fac664e04e493d6f6afc179ace5
Ryan Brinkman <ryanjbrinkman@outlook.com>
2024-11-09 18:57:25 -0600

    Updated various files.

---
+++ intellij/to-do-list-app/src/App.java
@@ -0,0 +1,12 @@
+/*
+ * Project: To Do List Application
+ * Author: Ryan Brinkman
+ * Date: 11/07/2024
+ * Repository: https://github.com/RyanBrin/java/tree/main/intellij/to-do-list-app
+ */
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
