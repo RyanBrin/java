434fe86a0fa0288f772d29d2c890df12ddd5133e
Ryan Brinkman <ryanjbrinkman@outlook.com>
2024-11-09 19:41:22 -0700

    Continued work on 'to-do-list-app.'

--- intellij/to-do-list-app/src/App.java
+++ intellij/to-do-list-app/src/App.java
@@ -5,8 +5,25 @@
  * Repository: https://github.com/RyanBrin/java/tree/main/intellij/to-do-list-app
  */
 
-public class App {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("To-Do List Application");
+
+        // Create UI elements here (e.g., Labels, Buttons)
+
+        Scene scene = new Scene(new Label("Hello, To-Do List!"), 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
+
