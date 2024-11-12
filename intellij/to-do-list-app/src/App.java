a8190cd1796badc6ef09585a904db4d66ee8466a
Ryan Brinkman <ryanjbrinkman@outlook.com>
2024-11-12 11:06:52 -0600

    Continued work on 'to-do-list-app.'

--- intellij/to-do-list-app/src/App.java
+++ intellij/to-do-list-app/src/App.java
@@ -9,11 +9,10 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
@@ -22,23 +21,21 @@ public class App extends Application {
     public void start(Stage primaryStage) {
 
         // Create a button
-        Button btn = new Button("Click Me!");
-        btn.setOnAction(e -> System.out.println("Hello, JavaFX!"));
+        Button clickMeButton = new Button("Click Me!");
+        clickMeButton.setOnAction(e -> System.out.println("Hello, JavaFX!"));
 
         // Create a label
-        Label label = new Label("a label");
-
+        Label messageLabel = new Label("A Label");
 
         // Set up the layout
-        StackPane root = new StackPane();
-        root.getChildren().addAll(btn, label);
+        BorderPane layout = new BorderPane();
+        layout.setTop(messageLabel);
+        layout.setCenter(clickMeButton);
 
         // Create the scene and set it to the stage
-        Scene scene = new Scene(root, 500, 500);
+        Scene scene = new Scene(layout, 400, 400);
         primaryStage.setTitle("To Do List App");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
-
-
