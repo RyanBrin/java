a9d1fcbd55c68d0751a4e814352f5d36b7ea06d3
Ryan Brinkman <rybrinkman@ms.bpsapps.org>
2024-12-10 11:51:19 -0600

    Made small edit to project 'Bank.'

--- intellij/bank/src/Bank.java
+++ intellij/bank/src/Bank.java
@@ -1,3 +1,10 @@
+/*
+ * Project: Bank
+ * Author: Ryan Brinkman
+ * Date: 11/13/2024
+ * Repository: https://github.com/RyanBrin/java/tree/main/intellij/bank/
+ */
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
